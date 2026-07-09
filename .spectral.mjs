0c09d82305e9d5e79160bad155878b8228a514b9
sharki@apifox.com <sharki@apifox.com>
2026-07-09 18:55:01 +0800

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMxNjAyNA/spectral.js?branch_slug=main&token=478412bb-c5f1-465e-8d07-3fedc51d0ec8";
+export default { extends: ruleset };
